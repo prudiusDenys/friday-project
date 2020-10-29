49c8809d7347e5cba658dd7fe3b5e6c91459c975
prudiusDenys <prdslife@gmail.com>
2020-10-29 15:49:17 +0100

    fix initialize process

--- src/BLL/store.ts
+++ src/BLL/store.ts
@@ -13,8 +13,6 @@ const reducers = combineReducers({
 })
 
 export type rootReducers = ReturnType<typeof reducers>
-
-
 export const store = createStore(reducers, applyMiddleware(thunkMiddleware))
 
 // @ts-ignore
