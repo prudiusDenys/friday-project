6bf4771cd3a3404d761f3f855626a3bbe0ba5478
prudiusDenys <prdslife@gmail.com>
2020-10-29 19:31:12 +0100

    Updates

---
+++ precache-manifest.de6c41f8e74ebe3b3f52f1302191e5ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "775865383f6a658a7900906c37e1544b",
+    "url": "/friday-project/index.html"
+  },
+  {
+    "revision": "221eaf9fe6d3fb315650",
+    "url": "/friday-project/static/css/main.60164eeb.chunk.css"
+  },
+  {
+    "revision": "bdc42ed0ee74961dc001",
+    "url": "/friday-project/static/js/2.40a911b2.chunk.js"
+  },
+  {
+    "revision": "44d582a97d02337b347d751d7baba8fd",
+    "url": "/friday-project/static/js/2.40a911b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "221eaf9fe6d3fb315650",
+    "url": "/friday-project/static/js/main.c9389ef1.chunk.js"
+  },
+  {
+    "revision": "6eb8990ea2c45aecee34",
+    "url": "/friday-project/static/js/runtime-main.124a8b0b.js"
+  },
+  {
+    "revision": "cceb10736cfadae43dc93c48b3ce7327",
+    "url": "/friday-project/static/media/spinner.cceb1073.svg"
+  }
+]);
