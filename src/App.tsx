e0c27ce6ebff7532785af5267ef55aedd87fad55
prudiusDenys <prdslife@gmail.com>
2020-10-30 21:31:02 +0100

    Login fix

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App = () => {
 		window.gapi.load('auth2', function() {
 			// @ts-ignore
 			window.gapi.auth2.init({
-				client_id: '192242024154-69i6pvffeplgds21p5kofofgrs2hmh1o.apps.googleusercontent.com'
+				client_id: '192242024154-3lqo1cg32i5v14jedhhrtrn0ima9jjap.apps.googleusercontent.com'
 			}).then(()=> console.log('OK'), ()=>{console.log('Error')})
 		});
 	},[])
